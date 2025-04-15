45e8a4c51ba4275c1e5512ce36f5ae621ff99d13
AnasAttar10 <anasattar2010@gmail.com>
2025-04-15 17:55:10 +0300

    now you can add product to wishlist without login

--- src/routes/wishlistRoute.ts
+++ src/routes/wishlistRoute.ts
@@ -3,13 +3,14 @@ import {
   addProductToWishlist,
   getLoggedUserWishlist,
   removeProductFromWishlist,
+  syncWishlistAfterLogin,
 } from "@services/wishlistService";
 import express from "express";
 
 const router = express.Router();
 
 router.use(protect, allowedTo("user"));
-
+router.post("/sync", syncWishlistAfterLogin);
 router.get("/", getLoggedUserWishlist);
 router.post("/", addProductToWishlist);
 router.delete("/:productId", removeProductFromWishlist);
--- src/services/wishlistService.ts
+++ src/services/wishlistService.ts
@@ -3,6 +3,7 @@ import { CustomRequest } from "../types/costumeRequest";
 import { Request, Response, NextFunction } from "express";
 import expressAsyncHandler from "express-async-handler";
 import Product from "@models/product";
+import ApiError from "@utils/apiError";
 
 // @desc    Add product to wishlist
 // @route   POST /api/v1/wishlist
@@ -54,6 +55,33 @@ export const getLoggedUserWishlist = expressAsyncHandler(
     res.status(200).json({
       status: "success",
       results: user?.wishlist.length,
+      data: user?.wishlist,
+    });
+  }
+);
+
+// @desc    Add localStorageProducts to  wishlist
+// @route   POST /api/v1/wishlist/sync
+// @access  Private/User
+export const syncWishlistAfterLogin = expressAsyncHandler(
+  async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const wishlistItems: string[] = req.body;
+
+    if (!Array.isArray(wishlistItems) || wishlistItems.length === 0) {
+      return next(new ApiError(`No items to sync`, 404));
+    }
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $addToSet: { wishlist: { $each: wishlistItems } },
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      status: "success",
+      numOfCartItems: user?.wishlist.length,
       data: user?.wishlist,
     });
   }
